af6ac468b49ca637f70a8d6e8c398ade1263c0a0
soheil babaee <soheilbabaee021@gmail.com>
2023-11-21 19:00:53 +0330

    this my Assignment

---
+++ Assignment.py
@@ -0,0 +1,134 @@
+# تمامی تمارین در یک جا نوشته شده است. لطفا برای تست کردن تمرین ها ، اون هارو از کامنت دربیارین
+# سهیل بابایی (Soheil Babaee)
+
+
+
+ 
+# "Hello, World!" ***********************************************************************************
+ 
+# print ("hello world")
+
+# "Hello, World!" ***********************************************************************************
+
+
+
+
+
+
+# Basic Calculator  ***********************************************************************************
+
+# print ("*************** Basic Calculator ************ ")
+# print ( "Enter tow number:" )
+# num1 = float(input())
+# num2 = float(input())
+# print ("Next step, choose your operation:")
+# print(" 1 => Total ")
+# print (" 2 => minus")
+# print (" 3 => multiplication ")
+# print (" 4 => Division ")
+# operation = input()
+# if operation == '1':
+#     print("Result: ", num1 + num2)
+# elif operation == '2':
+#     print("Result: ", num1 - num2)
+# elif operation == '3':
+#     print("Result: ", num1 * num2)
+# elif operation == '4':
+#     if num2 != 0: 
+#         print("Result: ", num1 / num2)
+#     else:
+#         print("Error: Division by zero is not possible.")
+# else:
+#     print("Error: Invalid operation.")
+
+# Basic Calculator  ***********************************************************************************
+
+
+
+
+
+
+
+
+# FizzBuzz ***********************************************************************************
+
+# for numbers in range(1, 101):
+#     if numbers % 3 == 0 and numbers % 5 == 0:
+#         print("FizzBuzz")
+#     if numbers % 3 == 0:
+#         print("Fizz")
+#     if numbers % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(numbers)
+
+
+# FizzBuzz ***********************************************************************************
+
+
+
+
+
+
+
+# Array Manipulation ***********************************************************************************
+
+# numbers = [4, 7, 1, 9, 2, 8, 5, 3, 6, 10]
+
+# numbers_sum = sum(numbers)
+# print(" Sum of number : {}".format(numbers_sum))
+
+# numbers_max = max(numbers)
+# print(" Maximum of number {}".format(numbers_max))
+
+# numbers_upside_down = list(reversed(numbers))
+# print(" upside down of number {}".format(numbers_upside_down))
+
+# Array Manipulation ***********************************************************************************
+
+
+
+
+
+
+
+# String Palindrome Checker  ***********************************************************************************
+
+
+# def is_palindrome(word_or_number):
+#     word_or_number = word_or_number.replace(" ", "")
+#     word_or_number = word_or_number.lower()
+#     return word_or_number == word_or_number[::-1]
+
+# input_character = input("Please enter a number or a phrase:")
+# if is_palindrome(input_character):
+#     print(f"{input_character} is a palindrome.")
+# else:
+#     print(f"{input_character} is not a palindrome.")
+
+
+# String Palindrome Checker ***********************************************************************************
+
+
+
+# Basic Data Structure - Stack
+# OOP language ( شیء گرایی )
+# stack = []
+
+# stack.append(1)
+# stack.append(2)
+# stack.append(3)
+
+# print("stack is:", stack)
+
+# items_deleted = stack.pop()
+# print(" Deleted element is: ", items_deleted)
+# print(" updated stack:", stack)
+
+
+# Basic Data Structure - Stack
+
+
+
+
+# سهیل بابایی (Soheil Babaee)
